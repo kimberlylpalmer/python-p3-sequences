eefd784ba5d41f747dd2385e51035e682608baf2
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-12-11 06:38:14 -0800

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+    elif length == 1:
+        print("[0]")
+    else:
+        fib_seq = [0, 1]
+        for i in range(2, length):
+            fib_seq.append(fib_seq[-1] + fib_seq[-2])
+        print(fib_seq)
